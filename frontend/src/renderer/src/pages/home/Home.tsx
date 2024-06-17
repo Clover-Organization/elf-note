4225a6e911152ef391816ce5b8525f67d5bbd430
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-06-17 16:33:24 -0300

    feat: Add Plate UI components for paragraph, code line, fixed toolbar, highlight leaf, avatar, comment reply items, comment avatar, table row, blockquote, and code syntax leaf

--- frontend/src/renderer/src/pages/home/Home.tsx
+++ frontend/src/renderer/src/pages/home/Home.tsx
@@ -1,39 +1,26 @@
 /* eslint-disable prettier/prettier */
+import logo from '../../global/assets/img/logo_ofc.png'
 import { ModeToggle } from "@renderer/global/components/mode-toggle";
-import { Badge } from "@renderer/global/components/ui/badge";
 import { Button } from "@renderer/global/components/ui/button";
-import { Drawer, DrawerContent, DrawerDescription, DrawerHeader, DrawerTitle, DrawerTrigger } from "@renderer/global/components/ui/drawer";
-import { Input } from "@renderer/global/components/ui/input";
-import { Label } from "@renderer/global/components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@renderer/global/components/ui/select";
-import { Textarea } from "@renderer/global/components/ui/textarea";
 import {
-    Bird,
     Book,
     Bot,
     Code2,
-    CornerDownLeft,
     Folder,
     LifeBuoy,
-    Mic,
-    Paperclip,
-    Rabbit,
-    Settings,
     Settings2,
-    Share,
     SquareTerminal,
     SquareUser,
-    Triangle,
-    Turtle,
 } from "lucide-react";
+import { PlateEditor } from './components/Editor/PlateEditor';
 
 export function Home() {
     return (
         <div className="grid h-screen w-full pl-[53px]">
             <aside className="inset-y fixed  left-0 z-20 flex h-full flex-col border-r">
-                <div className="border-b p-[6px]">
+                <div className="border-b p-[6px] grid place-items-center">
                     <Button variant="outline" size="icon" aria-label="Home">
-                        <Triangle className="size-5 fill-foreground" />
+                        <img src={logo} className="size-5 fill-foreground h-full w-full" />
                     </Button>
                 </div>
                 <nav className="grid gap-1 p-2">
@@ -114,139 +101,10 @@ export function Home() {
                 </nav>
             </aside>
             <div className="flex flex-col">
-                <header className="sticky top-0 z-10 flex h-[53px] items-center gap-1 border-b bg-background px-4">
-                    <h1 className="text-xl font-semibold">Playground</h1>
-                    <Drawer>
-                        <DrawerTrigger asChild>
-                            <Button variant="ghost" size="icon" className="md:hidden">
-                                <Settings className="size-4" />
-                                <span className="sr-only">Settings</span>
-                            </Button>
-                        </DrawerTrigger>
-                        <DrawerContent className="max-h-[80vh]">
-                            <DrawerHeader>
-                                <DrawerTitle>Configuration</DrawerTitle>
-                                <DrawerDescription>
-                                    Configure the settings for the model and messages.
-                                </DrawerDescription>
-                            </DrawerHeader>
-                            <form className="grid w-full items-start gap-6 overflow-auto p-4 pt-0">
-                                <fieldset className="grid gap-6 rounded-lg border p-4">
-                                    <legend className="-ml-1 px-1 text-sm font-medium">
-                                        Settings
-                                    </legend>
-                                    <div className="grid gap-3">
-                                        <Label htmlFor="model">Model</Label>
-                                        <Select>
-                                            <SelectTrigger
-                                                id="model"
-                                                className="items-start [&_[data-description]]:hidden"
-                                            >
-                                                <SelectValue placeholder="Select a model" />
-                                            </SelectTrigger>
-                                            <SelectContent>
-                                                <SelectItem value="genesis">
-                                                    <div className="flex items-start gap-3 text-muted-foreground">
-                                                        <Rabbit className="size-5" />
-                                                        <div className="grid gap-0.5">
-                                                            <p>
-                                                                Neural{" "}
-                                                                <span className="font-medium text-foreground">
-                                                                    Genesis
-                                                                </span>
-                                                            </p>
-                                                            <p className="text-xs" data-description>
-                                                                Our fastest model for general use cases.
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                </SelectItem>
-                                                <SelectItem value="explorer">
-                                                    <div className="flex items-start gap-3 text-muted-foreground">
-                                                        <Bird className="size-5" />
-                                                        <div className="grid gap-0.5">
-                                                            <p>
-                                                                Neural{" "}
-                                                                <span className="font-medium text-foreground">
-                                                                    Explorer
-                                                                </span>
-                                                            </p>
-                                                            <p className="text-xs" data-description>
-                                                                Performance and speed for efficiency.
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                </SelectItem>
-                                                <SelectItem value="quantum">
-                                                    <div className="flex items-start gap-3 text-muted-foreground">
-                                                        <Turtle className="size-5" />
-                                                        <div className="grid gap-0.5">
-                                                            <p>
-                                                                Neural{" "}
-                                                                <span className="font-medium text-foreground">
-                                                                    Quantum
-                                                                </span>
-                                                            </p>
-                                                            <p className="text-xs" data-description>
-                                                                The most powerful model for complex
-                                                                computations.
-                                                            </p>
-                                                        </div>
-                                                    </div>
-                                                </SelectItem>
-                                            </SelectContent>
-                                        </Select>
-                                    </div>
-                                    <div className="grid gap-3">
-                                        <Label htmlFor="temperature">Temperature</Label>
-                                        <Input id="temperature" type="number" placeholder="0.4" />
-                                    </div>
-                                    <div className="grid gap-3">
-                                        <Label htmlFor="top-p">Top P</Label>
-                                        <Input id="top-p" type="number" placeholder="0.7" />
-                                    </div>
-                                    <div className="grid gap-3">
-                                        <Label htmlFor="top-k">Top K</Label>
-                                        <Input id="top-k" type="number" placeholder="0.0" />
-                                    </div>
-                                </fieldset>
-                                <fieldset className="grid gap-6 rounded-lg border p-4">
-                                    <legend className="-ml-1 px-1 text-sm font-medium">
-                                        Messages
-                                    </legend>
-                                    <div className="grid gap-3">
-                                        <Label htmlFor="role">Role</Label>
-                                        <Select defaultValue="system">
-                                            <SelectTrigger>
-                                                <SelectValue placeholder="Select a role" />
-                                            </SelectTrigger>
-                                            <SelectContent>
-                                                <SelectItem value="system">System</SelectItem>
-                                                <SelectItem value="user">User</SelectItem>
-                                                <SelectItem value="assistant">Assistant</SelectItem>
-                                            </SelectContent>
-                                        </Select>
-                                    </div>
-                                    <div className="grid gap-3">
-                                        <Label htmlFor="content">Content</Label>
-                                        <Textarea id="content" placeholder="You are a..." />
-                                    </div>
-                                </fieldset>
-                            </form>
-                        </DrawerContent>
-                    </Drawer>
-                    <Button
-                        variant="outline"
-                        size="sm"
-                        className="ml-auto gap-1.5 text-sm"
-                    >
-                        <Share className="size-3.5" />
-                        Share
-                    </Button>
-                </header>
                 <main className="grid flex-1 gap-4 overflow-auto p-4">
                     <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
-                        <Badge variant="outline" className="absolute right-3 top-3">
+                        <PlateEditor />
+                        {/* <Badge variant="outline" className="absolute right-3 top-3">
                             Output
                         </Badge>
                         <div className="flex-1" />
@@ -275,7 +133,7 @@ export function Home() {
                                     <CornerDownLeft className="size-3.5" />
                                 </Button>
                             </div>
-                        </form>
+                        </form> */}
                     </div>
                 </main>
             </div>

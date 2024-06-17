d713168eb26d610a53c8963cb9f5deca7ccda266
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-06-17 15:19:32 -0300

    refactor: Import Home component with destructuring in App.tsx

---
+++ frontend/src/renderer/src/global/components/mode-toggle.tsx
@@ -0,0 +1,33 @@
+import { Moon, Sun } from 'lucide-react';
+
+import { DropdownMenu, DropdownMenuTrigger } from './ui/dropdown-menu';
+import { Button } from './ui/button';
+import { useTheme } from './theme-provider';
+
+export function ModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          onClick={() => {
+            if (theme === 'dark') {
+              setTheme('light');
+              console.log(theme);
+            } else {
+              setTheme('dark');
+              console.log(theme);
+            }
+          }}
+          variant="outline"
+          size="icon"
+        >
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all text-primary dark:-rotate-90 dark:scale-0" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 text-primary transition-all dark:rotate-0 dark:scale-100" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+    </DropdownMenu>
+  );
+}
--- frontend/src/renderer/src/main.tsx
+++ frontend/src/renderer/src/main.tsx
@@ -2,9 +2,12 @@ import "./global/assets/css/main.css";
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { ThemeProvider } from "./global/components/theme-provider";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>
 )

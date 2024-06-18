a9c4a080ed5dff73aa5658a99c6970e14c11fbe3
RyanGustavoGoncalves <ryan_gustavo09@outlook.com>
2024-06-18 11:47:19 -0300

    refactor: Update import paths for Plate UI components

--- frontend/src/renderer/src/pages/home/components/Editor/MonacoEditor.tsx
+++ frontend/src/renderer/src/pages/home/components/Editor/MonacoEditor.tsx
@@ -3,23 +3,40 @@
 
 import React, { useRef, useEffect } from 'react';
 import * as monaco from 'monaco-editor/esm/vs/editor/editor.api';
-
-import MonacoEnvironment from './components/utils/MonacoEnvironment';
+import { useTheme } from '@renderer/global/components/theme-provider';
 
 interface MonacoEditorProps {
-    // Props do seu componente
     language: string;
     value: string;
-    onChange: (value: string) => void;
+    onChange?: (value: string) => void;
 }
 
-const MonacoEditor: React.FC<MonacoEditorProps> = ({ language, value, onChange }) => {
+const defaultOnChange = (value: string): void => {
+    console.log(value);
+};
+
+
+/**
+ * A custom Monaco Editor component.
+ * @param language - The language mode for the editor.
+ * @param value - The initial value of the editor.
+ * @param onChange - The callback function to be called when the content of the editor changes.
+ */
+const MonacoEditor: React.FC<MonacoEditorProps> = ({ language, value, onChange = defaultOnChange }) => {
+    const { theme, setTheme } = useTheme();
+    const setEditorTheme = (theme: string) => {
+        monaco.editor.setTheme(theme);
+    };
+
     const editorRef = useRef<HTMLDivElement | null>(null);
     let monacoEditor: monaco.editor.IStandaloneCodeEditor | null = null;
 
     useEffect(() => {
+        setEditorTheme(theme === 'dark' ? 'vs-dark' : 'vs-light');
+    }, [theme]);
+
+    useEffect(() => {
         if (editorRef.current) {
-            // Configuração do editor
             monacoEditor = monaco.editor.create(editorRef.current, {
                 value,
                 language,
@@ -35,7 +52,6 @@ const MonacoEditor: React.FC<MonacoEditorProps> = ({ language, value, onChange }
         }
 
         return () => {
-            // Limpar o editor
             if (monacoEditor) {
                 monacoEditor.dispose();
                 monacoEditor = null;
